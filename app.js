5aa6cf6f55635826c4e2f87445472090944b42b1
Suffyan101 <suffyan.r01@gmail.com>
2022-07-13 00:45:32 +0500

    Assignment 10

---
+++ app.js
@@ -0,0 +1,29 @@
+// Question 1
+// function dispalydate(){
+//     document.write(Date())
+// }
+
+// dispalydate();
+
+// Question 2
+
+// var a = prompt("Please enter your First Name");
+// var b = prompt("Please enter your Last Name");
+
+// function greet(){
+//     document.write("Welcome "+a+" "+b)
+// }
+// greet(a,b)
+
+// Question 3
+
+// var a = +prompt("Please enter First Number");
+// var b = +prompt("Please enter Second Number");
+// var c = a+b
+// function Sum(){
+//     document.write("The Sum of first and second number is "+ c)
+// }
+// Sum(a,b)
+
+// Question 4
+
